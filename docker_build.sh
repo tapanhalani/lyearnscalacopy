6ed5869d8918dc585a52c712c71ab6e409a90cd1
Tapan Halani <tapanhalani231@gmail.com>
2016-10-07 01:17:06 +0530

    building a docker image now...

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 tar zcf lyearn-backend.tar.gz .
-curl -v  --unix-socket /var/run/docker.sock -H 'Content-Type: application/tar' --data-binary @lyearn-backend.tar.gz  -X POST http:/build?t=tapanhalani/lyearn-backend.tar.gz
+curl -v  --unix-socket /var/run/docker.sock -H 'Content-Type: application/tar' --data-binary @lyearn-backend.tar.gz  -X POST http:/build?t=tapanhalani/lyearn-backend
