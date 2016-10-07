68fddca2c0661746fa25f434e12e3fff39f6c813
Tapan Halani <tapanhalani231@gmail.com>
2016-10-07 19:14:44 +0530

    push now with curl request.

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -2,4 +2,5 @@
 
 echo "hello tapan"
 pwd
-DOCKER_API_VERSION=1.22 sbt dockerBuildAndPush
+DOCKER_API_VERSION=1.22 sbt docker
+curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: eyB1c2VybmFtZTogdGFwYW5oYWxhbmksIHBhc3N3b3JkOiAyOG1hcmNoMTk5NCwgZW1haWw6IHRhcGFuaGFsYW5pMjMxQGdtYWlsLmNvbSB9Cg==' -X POST http:/images/tapanhalani/lyearnscalacopy/push?tag=latest
