02786823877942279de6c7e7a94c5723738738a7
Tapan Halani <tapanhalani231@gmail.com>
2016-10-10 14:15:09 +0530

    trying to solve error

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,4 +2,4 @@
 
 echo "hello tapan"
 pwd
-DOCKER_API_VERSION=1.22 docker run --rm  -it tapanhalani/rancherpythonapi query 1s9
+DOCKER_API_VERSION=1.22 docker run --rm tapanhalani/rancherpythonapi query 1s9
