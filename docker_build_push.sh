ea7822574dcdd6af3ff5467122f762879a0caedf
Tapan Halani <tapanhalani231@gmail.com>
2016-10-10 18:41:39 +0530

    integrating drone now.

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -3,4 +3,5 @@
 echo "hello tapan"
 pwd
 DOCKER_API_VERSION=1.22 sbt docker
+
 curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: eyAidXNlcm5hbWUiOiAidGFwYW5oYWxhbmkiLCAicGFzc3dvcmQiOiAiMjhtYXJjaDE5OTQiLCAiZW1haWwiOiAidGFwYW5oYWxhbmkyMzFAZ21haWwuY29tIiB9Cg==' -X POST http:/images/tapanhalani/lyearnscalacopy/push?tag=latest
--- docker_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "hello tapan"
-pwd
-DOCKER_API_VERSION=1.22 docker run --rm tapanhalani/rancherpythonapi query 1s9
-DOCKER_API_VERSION=1.22 docker run --rm tapanhalani/rancherpythonapi upgrade 1s9
