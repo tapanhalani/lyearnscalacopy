17ad00b21a392818380b3793ee1a1f6fb86fb59c
Tapan Halani <tapanhalani231@gmail.com>
2016-10-10 14:32:42 +0530

    a fix is running. checking on upgrading.

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -3,3 +3,4 @@
 echo "hello tapan"
 pwd
 DOCKER_API_VERSION=1.22 docker run --rm tapanhalani/rancherpythonapi query 1s9
+DOCKER_API_VERSION=1.22 docker run --rm tapanhalani/rancherpythonapi upgrade 1s9
